8cb4214a3ab9449c79013b38944184418bea88a5
noveltaker <pnw123456@icloud.com>
2022-07-22 18:12:58 +0900

    feat: csv read and database save example
    - csv 를 이용해서 itemReader 을 만들는 예제
    - itemWriter 를 이용해서 database 에 insert 예제

---
+++ src/main/kotlin/io/example/dataserving/dto/UserDTO.kt
@@ -0,0 +1,34 @@
+package io.example.dataserving.dto
+
+import io.example.dataserving.domain.User
+import java.util.Date
+
+data class UserDTO(
+    private var id: Long? = null,
+    // private var createdDate: Date? = null,
+    // private var updatedDate: Date? = null,
+    private var point: Double? = null,
+    private var email: String? = null,
+    private var gender: String? = null,
+    private var firstName: String? = null,
+    private var lastName: String? = null
+) {
+    fun getId() = this.id
+
+    // fun getCreatedDate() = this.createdDate
+    // fun getUpdatedDate() = this.updatedDate
+    fun getPoint() = this.point
+    fun getEmail() = this.email
+    fun getGender() = this.gender
+    fun getFirstName() = this.firstName
+    fun getLastName() = this.lastName
+
+    fun toEntity() =
+        User(
+            this.point,
+            this.email,
+            this.gender,
+            this.firstName,
+            this.lastName
+        )
+}
---
+++ src/main/kotlin/io/example/dataserving/job/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package io.example.dataserving.job.repository
+
+import io.example.dataserving.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+}
