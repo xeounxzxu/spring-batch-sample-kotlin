6bc7e7bc95ede9eff8ce0e2cd4c3222fdfea7d90
noveltaker <pnw123456@icloud.com>
2022-07-23 19:54:47 +0900

    feat: batch scheduler example

---
+++ src/main/kotlin/io/example/dataserving/job/scheduler/SimpleJob4Scheduler.kt
@@ -0,0 +1,29 @@
+package io.example.dataserving.job.scheduler
+
+import io.example.dataserving.job.incrementer.DateIncrementer
+import io.example.dataserving.utils.LogUtil
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class SimpleJob4Scheduler constructor(
+    private val jobLauncher: JobLauncher,
+    private val simpleJob4: Job,
+    private val logUtil: LogUtil
+) {
+
+    @Scheduled(initialDelay = 10000, fixedDelay = 3000 , cron = "0 0/5 * * *")
+    fun runJob() {
+
+        logUtil.getLogger().info("started scheduler >>")
+
+        val dateIncrementer = DateIncrementer()
+            .getNext(null)
+
+        jobLauncher.run(simpleJob4, dateIncrementer)
+
+        logUtil.getLogger().info("ended scheduler >>")
+    }
+}
