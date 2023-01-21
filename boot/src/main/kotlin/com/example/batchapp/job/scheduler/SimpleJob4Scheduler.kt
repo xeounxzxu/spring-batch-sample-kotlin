1e2122c43fc2c3fd00e4a7b6eb20f3c099c8db53
spark <pnw123456@icloud.com>
2023-01-21 23:11:29 +0900

    chore: #3 불필요한 스케줄러 주석

--- boot/src/main/kotlin/com/example/batchapp/job/scheduler/SimpleJob4Scheduler.kt
+++ boot/src/main/kotlin/com/example/batchapp/job/scheduler/SimpleJob4Scheduler.kt
@@ -1,29 +1,29 @@
 package com.example.batchapp.job.scheduler
 
-import com.example.batchapp.job.incrementer.DateIncrementer
-import com.example.batchapp.utils.LogUtil
-import org.springframework.batch.core.Job
-import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.stereotype.Component
-
-@Component
-class SimpleJob4Scheduler constructor(
-    private val jobLauncher: JobLauncher,
-    private val simpleJob4: Job,
-    private val logUtil: LogUtil
-) {
-
-    @Scheduled(cron = "* 0/2 * * * *")
-    fun runJob() {
-
-        logUtil.getLogger().info("started scheduler >>")
-
-        val dateIncrementer = DateIncrementer()
-            .getNext(null)
-
-        jobLauncher.run(simpleJob4, dateIncrementer)
-
-        logUtil.getLogger().info("ended scheduler >>")
-    }
-}
+//import com.example.batchapp.job.incrementer.DateIncrementer
+//import com.example.batchapp.utils.LogUtil
+//import org.springframework.batch.core.Job
+//import org.springframework.batch.core.launch.JobLauncher
+//import org.springframework.scheduling.annotation.Scheduled
+//import org.springframework.stereotype.Component
+//
+//@Component
+//class SimpleJob4Scheduler constructor(
+//    private val jobLauncher: JobLauncher,
+//    private val simpleJob4: Job,
+//    private val logUtil: LogUtil
+//) {
+//
+//    @Scheduled(cron = "* 0/2 * * * *")
+//    fun runJob() {
+//
+//        logUtil.getLogger().info("started scheduler >>")
+//
+//        val dateIncrementer = DateIncrementer()
+//            .getNext(null)
+//
+//        jobLauncher.run(simpleJob4, dateIncrementer)
+//
+//        logUtil.getLogger().info("ended scheduler >>")
+//    }
+//}
