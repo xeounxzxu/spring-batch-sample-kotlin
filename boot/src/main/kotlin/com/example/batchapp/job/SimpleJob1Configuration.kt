bc0c3f7bfb7562c94d82ad4ffbc820de2bfb127e
spark <pnw123456@icloud.com>
2023-01-22 00:21:35 +0900

    feat: #6  Single Running Job add

--- boot/src/main/kotlin/com/example/batchapp/job/SimpleJob1Configuration.kt
+++ boot/src/main/kotlin/com/example/batchapp/job/SimpleJob1Configuration.kt
@@ -1,15 +1,10 @@
 package com.example.batchapp.job
 
-import com.example.batchapp.job.listener.SimpleJob1Listener
-import com.example.batchapp.job.listener.SimpleJob1OtherListener
 import com.example.batchapp.job.tasklet.SimpleJob1Task1
 import com.example.batchapp.utils.LogUtil
-import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration
 import org.springframework.batch.core.job.builder.JobBuilder
-import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
-import org.springframework.batch.core.step.tasklet.TaskletStep
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
@@ -18,29 +13,21 @@ import org.springframework.transaction.PlatformTransactionManager
 open class SimpleJob1Configuration constructor(
     private val logUtil: LogUtil,
     private val transactionManager: PlatformTransactionManager
-) : DefaultBatchConfiguration() {
+) {
+
 
     @Bean
-    open fun simpleJob1(jobRepository: JobRepository) =
-        JobBuilder("simpleJob1", jobRepository)
-            .listener(simpleJob1Listener())
-            .listener(simpleJob1OtherListener())
-            .incrementer(RunIdIncrementer())
-            .start(simpleStep1(jobRepository))
+    open fun runningJob(jobRepository: JobRepository) =
+        JobBuilder("runJob1", jobRepository)
+            .start(runStep1(jobRepository))
             .build()
 
     @Bean
-    open fun simpleStep1(jobRepository: JobRepository): TaskletStep =
-        StepBuilder("simpleStep1", jobRepository)
-            .tasklet(simpleJob1Task1(), transactionManager)
-            .build()
+    open fun runStep1(jobRepository: JobRepository) = StepBuilder("step1", jobRepository)
+        .tasklet(simpleJob1Task1(), transactionManager)
+        .build()
 
     @Bean
     open fun simpleJob1Task1() = SimpleJob1Task1(logUtil)
 
-    @Bean
-    open fun simpleJob1Listener() = SimpleJob1Listener(logUtil)
-
-    @Bean
-    open fun simpleJob1OtherListener() = SimpleJob1OtherListener(logUtil)
 }
