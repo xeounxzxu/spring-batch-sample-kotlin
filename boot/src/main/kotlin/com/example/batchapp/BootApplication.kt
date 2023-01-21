740a63e474fe5ed04b70f67a79a65219e20e78d3
spark <pnw123456@icloud.com>
2023-01-22 00:21:08 +0900

    chore: #5 trace for JPA DataSource Not Working And Batch Meta Table Not Created

--- boot/src/main/kotlin/com/example/batchapp/BootApplication.kt
+++ boot/src/main/kotlin/com/example/batchapp/BootApplication.kt
@@ -1,12 +1,10 @@
 package com.example.batchapp
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
-@EnableBatchProcessing
 @SpringBootApplication
 open class BootApplication
 
--- boot/src/main/kotlin/com/example/batchapp/config/AppDataSourceConfiguration.kt
+++ boot/src/main/kotlin/com/example/batchapp/config/AppDataSourceConfiguration.kt
@@ -3,26 +3,35 @@ package com.example.batchapp.config
 import com.example.batchapp.utils.LogUtil
 import com.zaxxer.hikari.HikariDataSource
 import org.h2.tools.Server
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.support.JdbcTransactionManager
 import java.sql.SQLException
+import javax.sql.DataSource
+
 
 @Configuration
+@EnableBatchProcessing(
+    dataSourceRef = "batchDataSource", transactionManagerRef = "batchTransactionManager"
+)
 open class AppDataSourceConfiguration constructor(
     private val logUtil: LogUtil
 ) {
 
-    @Primary
-    @Bean("dataSource")
+    @Bean
     @ConfigurationProperties("spring.datasource.hikari")
     @Throws(SQLException::class)
-    open fun appDataSource(): HikariDataSource? {
+    open fun batchDataSource(): DataSource {
         logUtil.getLogger().info("Started H2 DataBase ==============")
-
         Server.createTcpServer("-tcp", "-tcpAllowOthers", "-ifNotExists", "-tcpPort", 9095.toString() + "").start()
-
         return HikariDataSource()
     }
+
+    @Bean
+    open fun batchTransactionManager(
+        @Qualifier("batchDataSource") dataSource: DataSource
+    ): JdbcTransactionManager = JdbcTransactionManager(dataSource)
 }
