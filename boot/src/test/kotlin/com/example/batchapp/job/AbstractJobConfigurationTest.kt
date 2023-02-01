bd73551af345637dcf501f37431f66c7bd47624d
spark <pnw123456@icloud.com>
2023-02-01 22:15:49 +0900

    refactor: #14 테스트 케이스 인프라스트러처 추가

--- boot/src/test/kotlin/com/example/batchapp/job/AbstractJobConfigurationTest.kt
+++ boot/src/test/kotlin/com/example/batchapp/job/AbstractJobConfigurationTest.kt
@@ -1,10 +1,17 @@
 package com.example.batchapp.job
 
 import BatchDataSourceTestConfiguration
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.batch.test.JobLauncherTestUtils
 import org.springframework.batch.test.context.SpringBatchTest
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.jdbc.core.JdbcOperations
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import javax.sql.DataSource
 
 @SpringBatchTest
 @ContextConfiguration(
@@ -12,9 +19,21 @@ import org.springframework.test.context.ContextConfiguration
         BatchDataSourceTestConfiguration::class,
     ]
 )
+@Transactional(value = "mainTransactionManager", propagation = Propagation.NOT_SUPPORTED)
 abstract class AbstractJobConfigurationTest {
 
     @Autowired
     protected lateinit var jobLauncherTestUtils: JobLauncherTestUtils
+
+    @Autowired
+    @Qualifier("mainDataSource")
+    private lateinit var dataSource: DataSource
+
+    private lateinit var jdbcTemplate: JdbcOperations
+
+    @BeforeEach
+    fun init() {
+        this.jdbcTemplate = JdbcTemplate(dataSource)
+    }
 }
 
--- boot/src/test/kotlin/com/example/batchapp/job/RunningJob2ConfigurationTest.kt
+++ boot/src/test/kotlin/com/example/batchapp/job/RunningJob2ConfigurationTest.kt
@@ -8,11 +8,8 @@ import org.springframework.batch.core.Job
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 
-@SpringJUnitConfig(
-    classes = [RunningJob2Configuration::class, MainDataSourceTestConfiguration::class]
-)
-class RunningJob2ConfigurationTest : AbstractJobConfigurationTest() {
-
+@SpringJUnitConfig(classes = [RunningJob2Configuration::class, MainDataSourceTestConfiguration::class])
+open class RunningJob2ConfigurationTest : AbstractJobConfigurationTest() {
 
     @Test
     fun `csv 파일을 로드 해서 데이터 적제 Job 성공 테스트 케이스`(@Qualifier("runningJob2") job: Job) {
