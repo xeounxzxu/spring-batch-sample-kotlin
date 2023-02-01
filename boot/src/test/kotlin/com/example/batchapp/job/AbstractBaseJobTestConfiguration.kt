3f59ebf46122662107508aeffec16e7713367dfd
spark <pnw123456@icloud.com>
2023-02-01 22:47:17 +0900

    fix: #14 job1 test case not working
    - jdbc를 세팅 하지 않는 job 에 사이드이팩트 발생
    - 해당 부분을 추상화로 변경 및 공통화
    - Base Test Config 과 Job Config 로 변경

---
+++ boot/src/test/kotlin/com/example/batchapp/job/AbstractBaseJobTestConfiguration.kt
@@ -0,0 +1,21 @@
+package com.example.batchapp.job
+
+import BatchDataSourceTestConfiguration
+import org.springframework.batch.test.JobLauncherTestUtils
+import org.springframework.batch.test.context.SpringBatchTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+
+
+@SpringBatchTest
+@ContextConfiguration(
+    classes = [
+        BatchDataSourceTestConfiguration::class,
+    ]
+)
+abstract class AbstractBaseJobTestConfiguration {
+
+
+    @Autowired
+    protected lateinit var jobLauncherTestUtils: JobLauncherTestUtils
+}
