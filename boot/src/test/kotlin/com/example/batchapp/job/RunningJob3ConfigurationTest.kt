9f3e7dfea11c902ff5273b989c69c0b125614365
spark <pnw123456@icloud.com>
2023-02-02 21:11:46 +0900

    test: #16 job test case example code add
    Close #16

---
+++ boot/src/test/kotlin/com/example/batchapp/job/RunningJob3ConfigurationTest.kt
@@ -0,0 +1,35 @@
+package com.example.batchapp.job
+
+import com.example.batchapp.config.AbstractJobTestConfiguration
+import com.example.batchapp.config.MainDataSourceTestConfiguration
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.batch.core.BatchStatus
+import org.springframework.batch.core.Job
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+
+@SpringJUnitConfig(
+    classes = [
+        RunningJob3Configuration::class,
+        MainDataSourceTestConfiguration::class
+    ]
+)
+class RunningJob3ConfigurationTest : AbstractJobTestConfiguration() {
+
+    @Test
+    fun `Running 잡 성공 테스트 케이스`(@Qualifier("running3Job") job: Job) {
+
+        jobLauncherTestUtils.job = job
+
+        val execution = jobLauncherTestUtils.launchJob()
+
+        assertEquals(BatchStatus.COMPLETED, execution.status)
+
+        val stepExecution = execution.stepExecutions.iterator().next()
+
+        assertEquals(BatchStatus.COMPLETED, stepExecution.status)
+        assertEquals(3, stepExecution.readCount)
+        assertEquals(3, stepExecution.writeCount)
+    }
+}
