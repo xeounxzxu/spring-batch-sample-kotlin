472c6a2649d9f4c9133eb72b7b627fb3f539f1a6
spark <pnw123456@icloud.com>
2023-02-02 20:16:29 +0900

    refactor: #15 userRepository add
    - 적재 카운트 체크 추가

--- boot/src/test/kotlin/com/example/batchapp/job/RunningJob2TestConfiguration.kt
+++ boot/src/test/kotlin/com/example/batchapp/job/RunningJob2TestConfiguration.kt
@@ -1,23 +1,32 @@
 package com.example.batchapp.job
 
 import com.example.batchapp.config.MainDataSourceTestConfiguration
+import com.example.batchapp.repository.UserRepository
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.batch.core.ExitStatus
 import org.springframework.batch.core.Job
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 
 @SpringJUnitConfig(classes = [RunningJob2Configuration::class, MainDataSourceTestConfiguration::class])
-open class RunningJob2TestConfiguration : AbstractJobTestConfiguration() {
+open class `RunningJob2 에서` : AbstractJobTestConfiguration() {
+
+    @Autowired
+    lateinit var userRepository: UserRepository
 
     @Test
-    fun `csv 파일을 로드 해서 데이터 적제 Job 성공 테스트 케이스`(@Qualifier("runningJob2") job: Job) {
+    fun `성공 테스트 케이스`(@Qualifier("runningJob2") job: Job) {
 
         jobLauncherTestUtils.job = job
 
         val status = jobLauncherTestUtils.launchJob()
 
         assertEquals(ExitStatus.COMPLETED.exitCode, status.exitStatus.exitCode)
+
+        val cnt = userRepository.count()
+
+        assertEquals(1001, cnt)
     }
 }
