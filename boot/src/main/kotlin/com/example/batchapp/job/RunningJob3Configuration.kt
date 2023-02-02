ff4bf4cbc1bec7f88a4848c9c119f8cb6ec1786d
spark <pnw123456@icloud.com>
2023-02-02 20:53:01 +0900

    feat: #16 테스트 케이스를 위한 로직 추가

---
+++ boot/src/main/kotlin/com/example/batchapp/job/RunningJob3Configuration.kt
@@ -0,0 +1,43 @@
+package com.example.batchapp.job
+
+import org.slf4j.LoggerFactory
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.core.step.tasklet.TaskletStep
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.support.ListItemReader
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+
+@Configuration
+open class RunningJob3Configuration(
+    @Qualifier("mainTransactionManager")
+    private val transactionManager: PlatformTransactionManager
+) {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Bean
+    open fun itemReader(): ListItemReader<String> = ListItemReader(listOf("foo", "bar", "baz"))
+
+    @Bean
+    open fun itemWriter(): ItemWriter<String> = ItemWriter { s ->
+        logger.info(s.toString())
+    }
+
+    @Bean
+    open fun running3Step1(jobRepository: JobRepository): TaskletStep =
+        StepBuilder("", jobRepository)
+            .chunk<String, String>(10, transactionManager)
+            .reader(itemReader())
+            .writer(itemWriter())
+            .build()
+
+    @Bean
+    open fun running3Job(jobRepository: JobRepository) = JobBuilder("", jobRepository)
+        .start(running3Step1(jobRepository))
+        .build()
+}
